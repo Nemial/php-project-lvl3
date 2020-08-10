ef693a03c969a594e4a84a7c116be8565c29e546
nemial <nemtre@protonmail.com>
2020-08-10 14:30:29 +0200

    Code fix

--- routes/web.php
+++ routes/web.php
@@ -68,10 +68,11 @@ Route::get(
     function () {
         $domains = DB::table('domains')->get(['id', 'name']);
         $checks = DB::table('domain_checks')
+            ->select(DB::raw('DISTINCT ON ("domain_id") domain_id, created_at, status_code'))
             ->orderBy('domain_id')
             ->orderBy('created_at', 'desc')
             ->distinct('domain_id')
-            ->get(['domain_id', 'created_at', 'status_code'])->keyBy('domain_id')->toArray();
+            ->get()->keyBy('domain_id')->toArray();
         return view('domains/index', ['domains' => $domains, 'checks' => $checks]);
     }
 )->name("domains");
